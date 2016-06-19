3efed2ab336d24d376a0e91dbd2ec92d407426be
Sashe Klechkovski <sasko_dh@hotmail.com>
2016-06-19 21:22:14 +0200

    Export @Profile decorator

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export {ComponentScan} from "./lib/decorators/ComponentScanDecorator";
 export {Configuration} from "./lib/decorators/ConfigurationDecorator";
 export {Controller} from "./lib/decorators/ControllerDecorator";
 export {Inject, Value} from "./lib/decorators/InjectionDecorators";
+export {Profile} from "./lib/decorators/ComponentDecorator";
 export {PropertySource} from "./lib/decorators/PropertySourceDecorator";
 export {RequestMapping, RequestMappingConfig, RequestMethod} from "./lib/decorators/RequestMappingDecorator";
 
