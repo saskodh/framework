554a3b00f57ecc6335cb753e0b6ef3f7a79ef38a
Dragan Andonovski <Dragan.Andonovski@netcetera.com>
2016-07-01 14:20:32 +0200

    [no-issue] Added support for passing plain types as parameters in a configuration file - added support also for passing arrays => values will be treated as string and will be concatenated with colon [reviewed by @saskodh]

--- test/lib/helpers/GeneralUtils.spec.ts
+++ test/lib/helpers/GeneralUtils.spec.ts
@@ -6,8 +6,7 @@ describe('GeneralUtils', function () {
     it('should flatten object', function () {
         // given
         let givenObject = {
-            a: { b: 'b', c: { d: 'd', e: 'e' } },
-            o: 'o'
+            a: { b: 'b', c: { d: 'd', e: 'e' } }
         };
 
         // when
@@ -17,7 +16,36 @@ describe('GeneralUtils', function () {
         expect(flattened.get('a.b')).eq('b');
         expect(flattened.get('a.c.d')).eq('d');
         expect(flattened.get('a.c.e')).eq('e');
-        expect(flattened.get('o')).eq('o');
+    });
+    
+    it('should work with arrays', function () {
+        // given
+        let givenObject = {
+            array: ['dev', 'prev', 'prep']
+        };
+        
+        // when
+        let flattened = GeneralUtils.flattenObject(givenObject);
+        
+        // then
+        expect(flattened.get('array')).eq('dev,prev,prep');
+    });
+    
+    it('should work with numbers, string, booleans..', function () {
+        // given
+        let givenObject = {
+            number: 1,
+            boolean: true,
+            string: 'string'
+        };
+        
+        // when
+        let flattened = GeneralUtils.flattenObject(givenObject);
+        
+        // then
+        expect(flattened.get('number')).eq(1);
+        expect(flattened.get('boolean')).eq(true);
+        expect(flattened.get('string')).eq('string');
     });
 
 });
