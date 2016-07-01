278e1c229a77c48df0cf3383ff07174fbdd533c7
Dragan Andonovski <draganandonovski995@gmail.com>
2016-07-01 14:25:10 +0200

    [no-issue] Removed forgotten console.log statements

--- src/lib/helpers/GeneralUtils.ts
+++ src/lib/helpers/GeneralUtils.ts
@@ -4,14 +4,11 @@ export class GeneralUtils {
 
     static flattenObject (ob): Map<string, string> {
         var flatMap: Map<string, string> = new Map();
-        console.log('parsing object', ob);
         _.forEach(ob, (value, prop) => {
-            console.log('for each: ', value, _.isObject(value), _.isArray(value));
             if (_.isPlainObject(value)) {
                 this.flattenObject(value)
                     .forEach((value, key) => flatMap.set(`${prop}.${key}`, value));
             } else if (_.isArray(value)) {
-                console.log('parsing array', value, value.join(','));
                 flatMap.set(prop, value.join(','));
             } else {
                 flatMap.set(prop, value);
