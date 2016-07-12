7b981b8aee7ccf8cc4f09b20199291e3e65510d9
Damjan Angelovski <Damjan.Angelovski@netcetera.com>
2016-07-12 15:36:42 +0200

    [#6] Enable injecting a list of dependencies - added Preconditions and TypeUtils helpers

---
+++ src/lib/helpers/TypeUtils.ts
@@ -0,0 +1,22 @@
+import {Preconditions} from "./validation/Preconditions";
+
+export class TypeUtils {
+
+    /**
+     * Checks if the given type is the same or extends the given comparison type.
+     *
+     * @param givenType the given type
+     * @param comparisonType the comparison type
+     * @returns Boolean
+     * @throws Error if any of the arguments is undefined
+     * */
+    static isA(givenType, comparisonType) {
+        Preconditions.assertDefined(givenType);
+        Preconditions.assertDefined(comparisonType);
+
+        if (givenType === comparisonType) {
+            return true;
+        }
+        return givenType.prototype instanceof comparisonType;
+    }
+}
