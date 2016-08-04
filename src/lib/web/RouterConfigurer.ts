9e182af8cf5d906f44a65da3a8c5c1b7b0adb958
Damjan Angelovski <damjangelovski@gmail.com>
2016-08-04 14:10:09 +0200

    [#28] Add support for @Interceptors - fixed todos (fixed bug and added tests)

--- src/lib/web/RouterConfigurer.ts
+++ src/lib/web/RouterConfigurer.ts
@@ -1,7 +1,6 @@
 import * as _ from "lodash";
 import { Router } from "express-serve-static-core";
 import { OrderUtil } from "../decorators/OrderDecorator";
-import { Interceptor } from "../decorators/InterceptorDecorator";
 import { RouterConfigItem } from "../decorators/RequestMappingDecorator";
 
 /**
@@ -23,7 +22,7 @@ export class RouterConfigurer {
      * Registers an interceptor.
      * @param instance instance of the interceptor
      * */
-    registerInterceptor(instance: Interceptor) {
+    registerInterceptor(instance) {
         this.interceptors.push(instance);
     }
 
@@ -33,16 +32,7 @@ export class RouterConfigurer {
      * @param handler the instance responsible for handling the route
      * */
     registerHandler(route: RouterConfigItem, handler) {
-        console.log(`Registering route. Path: '${route.requestConfig.path}', method: ${route.requestConfig.method}.`);
-        this.router[route.requestConfig.method](route.requestConfig.path, this.wrap(async (request, response, next) => {
-            let result = await handler[route.methodHandler](request, response);
-            // TODO #3 saskodh: Check whether is more convenient to store in the request zone or pass on next
-            response.$$frameworkData = {
-                view: route.view,
-                model: result
-            };
-            next();
-        }));
+        this.routeHandlers.set(route, handler);
     }
 
     /**
@@ -55,7 +45,6 @@ export class RouterConfigurer {
     }
 
     private configureMiddlewares() {
-        // TODO #28: bug route handlers are still executed before preHandlers
         this.router.use(this.wrap(this.preHandler.bind(this)));
         // NOTE: we will have our middleware handler when we drop the dependency to express
         // That would require the dispatching by path to be implemented on our side
@@ -68,8 +57,8 @@ export class RouterConfigurer {
         for (let [route, handler] of this.routeHandlers.entries()) {
             let httpMethod = route.requestConfig.method;
             let path = route.requestConfig.path;
+            console.log(`Registering route. Path: '${path}', method: ${httpMethod}.`);
 
-            // console.log(`Registering route. Path: '${path}', method: ${httpMethod}.`);
             this.router[httpMethod](path, this.wrap(async(request, response, next) => {
                 let result = await handler[route.methodHandler](request, response);
                 // TODO #3 saskodh: Check whether is more convenient to store in the request zone or pass on next
--- test/lib/web/RouterConfigurer.spec.ts
+++ test/lib/web/RouterConfigurer.spec.ts
@@ -1,23 +1,21 @@
 import { expect } from "chai";
-import { stub, spy } from "sinon";
+import { stub, spy, match } from "sinon";
 import { Router } from "express";
 import { Interceptor } from "../../../src/lib/decorators/InterceptorDecorator";
 import { RouterConfigurer } from "../../../src/lib/web/RouterConfigurer";
 import { OrderUtil } from "../../../src/lib/decorators/OrderDecorator";
+import { RouterConfigItem, RequestMethod } from "../../../src/lib/decorators/RequestMappingDecorator";
 
-// TODO #28: add missing tests
 describe('RouterConfigurer', function () {
 
     @Interceptor()
-    class PreHandleInterceptor implements Interceptor {
-        preHandle() {} // tslint:disable-line
-        postHandle(request, response) {} // tslint:disable-line
+    class PreHandleInterceptor {
+        preHandle(request, response) {} // tslint:disable-line
     }
 
     @Interceptor()
-    class PostHandleInterceptor implements Interceptor {
-        preHandle(request, response) {} // tslint:disable-line
-        postHandle() {} // tslint:disable-line
+    class PostHandleInterceptor {
+        postHandle(request, response) {} // tslint:disable-line
     }
 
     let router: Router;
@@ -35,6 +33,7 @@ describe('RouterConfigurer', function () {
     it('should initialize properly', async function () {
         // given / when / then
         expect((<any> routerConfigurer).interceptors).to.eql([]);
+        expect((<any> routerConfigurer).routeHandlers).to.be.instanceOf(Map);
     });
 
     it('should register interceptors', async function () {
@@ -52,9 +51,24 @@ describe('RouterConfigurer', function () {
         expect((<any> routerConfigurer).interceptors).to.include(instanceB);
     });
 
+    it('should register the handlers', async function () {
+        // given
+        let stubOnSetRouteHandler = stub((<any> routerConfigurer).routeHandlers, 'set');
+        let routerConfigItem = new RouterConfigItem({path: 'path'}, 'handler');
+        // when
+        routerConfigurer.registerHandler(routerConfigItem, 'handler');
+
+        // then
+        expect(stubOnSetRouteHandler.calledOnce).to.be.true;
+        expect(stubOnSetRouteHandler.calledWith(routerConfigItem, 'handler')).to.be.true;
+
+        stubOnSetRouteHandler.restore();
+    });
+
     it('should configure the router', async function () {
         // given
         let stubOnOrderList = stub(OrderUtil, 'orderList').returns(['firstInterceptor', 'secondInterceptor']);
+        let stubOnConfigureMiddlewares = stub(routerConfigurer, 'configureMiddlewares');
 
         // when
         routerConfigurer.configure();
@@ -63,6 +77,96 @@ describe('RouterConfigurer', function () {
         expect(stubOnOrderList.calledOnce).to.be.true;
         expect(stubOnOrderList.calledWith([])).to.be.true;
         expect((<any> routerConfigurer).interceptors).to.eql(['firstInterceptor', 'secondInterceptor']);
+        expect(stubOnConfigureMiddlewares.calledOnce).to.be.true;
+
+        stubOnOrderList.restore();
+        stubOnConfigureMiddlewares.restore();
+    });
+
+    it('should configure middlewares on router', async function () {
+        // given
+        let stubOnUse = stub((<any> routerConfigurer).router, 'use');
+        let stubOnRegisterRouteHandlers = stub(routerConfigurer, 'registerRouteHandlers');
+        let stubOnPreHandler = stub((<any> routerConfigurer).preHandler, 'bind')
+            .returns('bound preHandler');
+        let stubOnPostHandler = stub((<any> routerConfigurer).postHandler, 'bind')
+            .returns('bound postHandler');
+        let stubOnResolver = stub((<any> routerConfigurer).resolver, 'bind')
+            .returns('bound resolver');
+        let stubOnWrap = stub((<any> routerConfigurer), 'wrap');
+        stubOnWrap.withArgs('bound preHandler').returns('wraped preHandler');
+        stubOnWrap.withArgs('bound postHandler').returns('wraped postHandler');
+
+        // when
+        (<any> routerConfigurer).configureMiddlewares();
+
+        // then
+        expect(stubOnUse.calledThrice).to.be.true;
+        expect(stubOnUse.args).to.eql([['wraped preHandler'], ['wraped postHandler'], ['bound resolver']]);
+        expect(stubOnRegisterRouteHandlers.calledOnce).to.be.true;
+
+        stubOnUse.restore();
+        stubOnRegisterRouteHandlers.restore();
+        stubOnPreHandler.restore();
+        stubOnPostHandler.restore();
+        stubOnResolver.restore();
+        stubOnWrap.restore();
+    });
+
+    it('should register methods on routes', async function () {
+        // given
+        class A {
+
+            get(request, response): Promise<any> {
+                return Promise.resolve('GET resolved');
+            }
+
+            post(request, response): Promise<any> {
+                return Promise.resolve('POST resolved');
+            }
+        }
+        let instanceofA = new A();
+
+        (<any> routerConfigurer).routeHandlers.clear();
+        (<any> routerConfigurer).routeHandlers.set(
+            new RouterConfigItem({path: '/get', method: RequestMethod.GET}, 'get'), instanceofA);
+        let postConfig = new RouterConfigItem({path: '/post', method: RequestMethod.POST}, 'post');
+        postConfig.view = 'viewName';
+        (<any> routerConfigurer).routeHandlers.set(postConfig, instanceofA);
+        let spyOnRouterGet = stub(router, RequestMethod.GET);
+        let spyOnRouterPost = stub(router, RequestMethod.POST);
+        let spyOnNext = spy();
+        let mockResponseGet = {
+            $$frameworkData: undefined
+        };
+        let mockResponsePost = {
+            $$frameworkData: undefined
+        };
+        let spyOnControllerGet = spy(instanceofA, 'get');
+        let spyOnControllerPost = spy(instanceofA, 'post');
+
+        // when
+        (<any> routerConfigurer).registerRouteHandlers();
+        let getRequestCallback = spyOnRouterGet.args[0][1];
+        let postRequestCallback = spyOnRouterPost.args[0][1];
+
+        await getRequestCallback('requestGET', mockResponseGet, spyOnNext);
+        await postRequestCallback('requestPOST', mockResponsePost, spyOnNext);
+
+        // then
+        expect(spyOnRouterGet.calledWith('/get', match.func)).to.be.true;
+        expect(spyOnRouterPost.calledWith('/post', match.func)).to.be.true;
+        expect(spyOnControllerGet.calledWith('requestGET', mockResponseGet)).to.be.true;
+        expect(spyOnControllerPost.calledWith('requestPOST', mockResponsePost)).to.be.true;
+        expect(mockResponseGet.$$frameworkData.model).to.be.eql('GET resolved');
+        expect(mockResponseGet.$$frameworkData.view).to.be.undefined;
+        expect(mockResponsePost.$$frameworkData.model).to.be.eql('POST resolved');
+        expect(mockResponsePost.$$frameworkData.view).to.be.eql('viewName');
+        expect(spyOnNext.calledTwice).to.be.true;
+
+        // cleanup
+        spyOnRouterGet.restore();
+        spyOnRouterPost.restore();
     });
 
     it('should pre handle', async function () {
@@ -103,5 +207,36 @@ describe('RouterConfigurer', function () {
 
         spyOnPreHandle.restore();
         spyOnPostHandle.restore();
+    });
+
+    it('should resolve response', async function () {
+        // given
+        let spyOnNext = spy();
+        let mockResponseGet = {
+            $$frameworkData: {
+                model: 'getResult',
+                view: undefined
+            },
+            json: spy(),
+            render: spy()
+        };
+        let mockResponsePost = {
+            $$frameworkData: {
+                model: 'postResult',
+                view: 'postView'
+            },
+            json: spy(),
+            render: spy()
+        };
+
+        // when
+        (<any> routerConfigurer).resolver('mockRequest', mockResponseGet, spyOnNext);
+        (<any> routerConfigurer).resolver('mockRequest', mockResponsePost, spyOnNext);
+
+        // then
+        expect(mockResponseGet.json.calledWith('getResult')).to.be.true;
+        expect(mockResponseGet.render.called).to.be.false;
+        expect(mockResponsePost.json.called).to.be.false;
+        expect(mockResponsePost.render.calledWith('postView', 'postResult')).to.be.true;
     });
 });
